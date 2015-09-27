0d7b7244a5fb0c9bf2ea6e1762e9e7896e428182
flamingfox <julien.brillet@yahoo.fr>
2015-09-27 18:07:56 +0200

    Doc ColorGradient OK

--- rendu/colorgradient.h
+++ rendu/colorgradient.h
@@ -6,31 +6,64 @@
 class ColorGradient
 {
 private:
-  struct ColorPoint  // Internal class used to store colors at different points in the gradient.
-  {
-    float r,g,b;      // Red, green and blue values of our color.
-    float val;        // Position of our color along the gradient (between 0 and 1).
-    ColorPoint(float red, float green, float blue, float value)
-      : r(red), g(green), b(blue), val(value) {}
-  };
-  std::vector<ColorPoint> color;      // An array of color points in ascending value.
+
+    /**
+    * @brief Internal class used to store colors at different points in the gradient.
+    */
+    struct ColorPoint
+    {
+        /** @brief r red value of our color. */
+        float r;
+        /** @brief g green value of our color. */
+        float g;
+        /** @brief b blue value of our color. */
+        float b;
+
+        /**
+         * @brief Position of our color along the gradient (between 0 and 1).
+         */
+        float val;
+
+        ColorPoint(float red, float green, float blue, float value)
+            : r(red), g(green), b(blue), val(value) {}
+    };
+
+    /**
+     * @brief An array of color points in ascending value.
+     */
+    std::vector<ColorPoint> color;
 
 public:
-  //-- Default constructor:
-  ColorGradient();
+    //-- Default constructor:
+    ColorGradient();
 
-  //-- Inserts a new color point into its correct position:
-  void addColorPoint(float red, float green, float blue, float value);
+    /**
+     * @brief Inserts a new color point into its correct position.
+     * @param[in] red red value of the color.
+     * @param[in] green green value of the color.
+     * @param[in] blue blue value of the color.
+     * @param[in] value position along the gradient.
+     */
+    void addColorPoint(float red, float green, float blue, float value);
 
-  //-- Inserts a new color point into its correct position:
-  void clearGradient();
+    /**
+     * @brief Inserts a new color point into its correct position.
+     */
+    void clearGradient();
 
-  //-- Places a 5 color heapmap gradient into the "color" vector:
-  void createDefaultHeatMapGradient();
+    /**
+     * @brief Places a 5 color heapmap gradient into the "color" vector.
+     */
+    void createDefaultHeatMapGradient();
 
-  //-- Inputs a (value) between 0 and 1 and outputs the (red), (green) and (blue)
-  //-- values representing that position in the gradient.
-  void getColorAtValue(const float value, float &red, float &green, float &blue) const;
+    /**
+     * @brief values representing that position in the gradient.
+     * @param[in] value a value between 0 and 1.
+     * @param[out] red red value of the color.
+     * @param[out] green green value of the color.
+     * @param[out] blue blue value of the color.
+     */
+    void getColorAtValue(const float value, float &red, float &green, float &blue) const;
 };
 
 #endif // COLORGRADIENT_H
