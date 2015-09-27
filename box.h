871a04f776620824017220212c3615e9d88da136
flamingfox <julien.brillet@yahoo.fr>
2015-09-27 15:09:47 +0200

    Doc box OK

--- box.h
+++ box.h
@@ -9,36 +9,120 @@
 
 using namespace Eigen;
 
+/**
+ * @brief Classe d'un objet géométrique de type boite. \n Il sert de boite englobante pour les objets.
+ */
 class Box
 {
 public:
-    Vector3f min, max;
+    /**
+     * @brief min Position d'un des points de la boite.
+     */
+    Vector3f min;
+    /**
+     * @brief max Position du point opposé au point \a min sur la boite.
+     */
+    Vector3f max;
+
     Box();
+
     Box(const Vector3f& _min, const Vector3f& _max);
-    //Box(const Mesh& mesh);
+
+    /**
+     * @brief Constructeur qui utilisera un ensemble de points pour calculer la boite englobante correspondante. \n
+     * Le constructeur passe le relai à la fonction parcourtPoints().
+     * @param[in] points Tableau de position de points d'un objet.
+     */
     Box(const std::vector<Vector3f>& points);
 
+    /**
+     * @brief Détermine si la boite englobante doit être modifiée pour comprendre le point \e p. \n
+     * Si c'est le cas la boite s'adapte pour contenir le point. \n
+     * Passe le relai à la méthode update().
+     * @param[in] p un point dans l'espace x, y, z.
+     */
     void updatePoint(const Vector3f& p);
+
+    /**
+     * @brief Test si le point \e p est dans l'espace compris de la boite.
+     * @param[in] p un point dans l'espace x, y, z.
+     * @return Le resultat du test. True = dedans, false = dehors.
+     */
     bool isIn(const Vector3f& p) const;
 
+    /**
+     * @brief Test si un rayon traverse la boite. \n
+     * Si c'est le cas, la methode retournera les distance d'intersection sur la boite. (cf. Rayon) \n
+     * Utilise la méthode intersectIn().
+     * @param[in] r Un rayon quelconque. (cf. Rayon)
+     * @param[out] distanceMin coefficient de distance du premier impacte sur la boite par rapport à l'origine du rayon. \n
+     * Le coefficient est dependant de la norme du vecteur direction du rayon.
+     * @param[out] distanceMax coefficient de distance du second impacte sur la boite par rapport à l'origine du rayon. \n
+     * Le coefficient est dependant de la norme du vecteur direction du rayon.
+     * @return Le resultat du test. True = intersection, false = pas d'intersection.
+     */
     bool intersect(const Rayon &r, float &distanceMin, float &distanceMax) const;
 
+    /**
+     * @brief Calcul la hauteur en \e z de la boite.
+     * @return la hauteur en \e z.
+     */
     float diffZ() const;
 
+    /**
+     * @brief Fait l'union entre deux boites, et redéfini min et max pour englober les deux boites.
+     * @param[in] box2 Une seconde boite.
+     */
     void merge(const Box& box2);
 
+    /**
+     * @brief Opère un translation rectiligne de la boite.
+     * @param[in] t Vecteur de translation de la boite.
+     */
     void operator+=(const Vector3f& t);
 
 protected:
 
 private:
+
+    /**
+     * @brief Initialise la boite avec des valeurs par defaut.
+     */
     inline void setDefaultBox();
+
+    /**
+     * @brief Détermine si la boite englobante doit être modifiée pour comprendre le point \e p. \n
+     * Si c'est le cas la boite s'adapte pour contenir le point. \n
+     * Passe le relai à la méthode updateMin() et updateMax().
+     * @param[in] p un point dans l'espace x, y, z.
+     */
     inline void update(const Vector3f& p);
+
+    /**
+     * @brief Détermine si le point min doit être modifiée pour comprendre le point \e p. \n
+     * Si c'est le cas le point min s'adapte pour contenir le point. \n
+     * @param[in] p un point dans l'espace x, y, z.
+     */
     inline void updateMin(const Vector3f& p);
+
+    /**
+     * @brief Détermine si le point max doit être modifiée pour comprendre le point \e p. \n
+     * Si c'est le cas le point max s'adapte pour contenir le point. \n
+     * @param[in] p un point dans l'espace x, y, z.
+     */
     inline void updateMax(const Vector3f& p);
 
+    /**
+     * @brief Détermine la positions de min et max pour englober tous les points de \e points.
+     * @param[in] Un ensemble de points.
+     */
     inline void parcourtPoints(const std::vector<Vector3f>& points);
 
+    /**
+     * @brief Test si le rayon r traverse la boite.
+     * @param[in] r Un rayon quelconque. (cf. Rayon)
+     * @return Le test d'intersection entre la boite et \e r.
+     */
     inline float intersectIn(const Rayon& r) const;
 };
 
